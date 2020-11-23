f621b6f2b5bc16e90b4e7052f847facfb21698d8
Hubz <33783503+Hubza@users.noreply.github.com>
2020-11-23 13:02:00 +0000

    its php

--- new/includes.php
+++ new/includes.php
@@ -3,10 +3,15 @@ $thisis = "php";
 function php(){
     $whatisthis = "it's php!";
 }
-
+php();
 ?>
 
 <link rel="canonical" href="https://archive.osu.hubza.co.uk" />
 <link rel="stylesheet" href="https://archive.osu.hubza.co.uk/new/main.css">
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet"
     media="screen" />
+
+
+    <?php
+$itis['still'] = "php";
+    ?>
