bd84a1e42b3718f28a5ce61fd65fc97e16c70742
Hubz <33783503+Hubza@users.noreply.github.com>
2020-11-23 12:34:40 +0000

    attempt 2

--- new/includes.php
+++ new/includes.php
@@ -1,3 +1,7 @@
+<?php
+$thisis = "php";
+?>
+
 <link rel="canonical" href="https://archive.osu.hubza.co.uk" />
 <link rel="stylesheet" href="https://archive.osu.hubza.co.uk/new/main.css">
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet"
