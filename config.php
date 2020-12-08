63447be813256dac0b32f107f7ce89cb20d11d12
Hubz <33783503+Hubza@users.noreply.github.com>
2020-12-08 11:29:22 +0000

    push placeholder password

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'c0osuarchive');
-define('DB_PASSWORD', 'kOm16g_8');
+define('DB_PASSWORD', 'placeholder');
 define('DB_NAME', 'c0osuarchive');
  
 // Create connection
