0a35a135368e3976a6d3328d4eb9a9c2e9351eea
Hubz <33783503+Hubza@users.noreply.github.com>
2020-12-20 18:29:19 +0000

    remove /old

--- old/includes.php
+++
@@ -1,3 +0,0 @@
-<link rel="canonical" href="https://archive.osu.hubza.co.uk" />
-<link rel="stylesheet" href="https://archive.osu.hubza.co.uk/new/main_new.css">
-    <script src="https://kit.fontawesome.com/91ad005f46.js" crossorigin="anonymous"></script>
