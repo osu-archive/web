858248c259e1b6c545668076d3a11afddf4dd7ca
Hubz <33783503+Hubza@users.noreply.github.com>
2020-12-20 18:30:36 +0000

    update api endpoint list

--- api/index.php
+++ api/index.php
@@ -19,9 +19,9 @@ status:   <a href="http://status.hubza.co.uk">http://status.hubza.co.uk</a>
 <hr>
 API endpoints
 
-<a>get version:</a>                    <a href="https://archive.osu.hubza.co.uk/new/api/version?v=b20191223.6">https://archive.osu.hubza.co.uk/new/api/version?v=b20191223.6</a>
-<a>get newest version:</a>             <a href="https://archive.osu.hubza.co.uk/new/api/latest">https://archive.osu.hubza.co.uk/new/api/latest</a>
-<a style="background-color: red; color: white">get list of all versions:</a>       <a href="https://archive.osu.hubza.co.uk/new/api/versions">https://archive.osu.hubza.co.uk/new/api/versions</a> / <a href="https://archive.osu.hubza.co.uk/new/api/versions?search=2019">https://archive.osu.hubza.co.uk/new/api/versions?search=b20191223.6</a>
+<a>get version:</a>                    <a href="https://archive.osu.hubza.co.uk/api/version?v=b20191223.6">https://archive.osu.hubza.co.uk/api/version?v=b20191223.6</a>
+<a>get newest version:</a>             <a href="https://archive.osu.hubza.co.uk/api/latest">https://archive.osu.hubza.co.uk/api/latest</a>
+<a>get list of all versions:</a>       <a href="https://archive.osu.hubza.co.uk/api/versions">https://archive.osu.hubza.co.uk/api/versions</a> / <a href="https://archive.osu.hubza.co.uk/api/versions?search=2019">https://archive.osu.hubza.co.uk/api/versions?search=b20191223.6</a>
 
 </pre>
 </body>
