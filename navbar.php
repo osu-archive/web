d97512c81e0297696158c63d57fcd7c958a169e9
Hubz <33783503+Hubza@users.noreply.github.com>
2020-12-20 19:03:49 +0000

    minecraft-like splashes as logo subtitle

--- navbar.php
+++ navbar.php
@@ -1,3 +1,7 @@
+<?php
+$f_contents = file("splash.txt"); 
+$line = $f_contents[rand(0, count($f_contents) - 1)];
+?>
 <!-- <div class="notice" style="background-color: #ee1111; padding:10px;">
     <p class="notice-text" style="color: white;">Work is being performed on the server, expect some blips in uptime.</p>
 </div>
@@ -7,7 +11,7 @@
         <img src="https://upload.hubza.co.uk/i/osuarchivelogo.svg" class="ht-logo">
 </a>
     <div class="header-bottom">
-        <p class="hb-text">Archiving all the osu! versions from 2007 to now!</hb>
+        <p class="hb-text"><?php echo $line; ?></hb>
         <div class="hb-right">
         <a style="text-decoration: none !important; color: white; font-size: 20px;" href="https://forms.gle/EByj5M6xvKy7WuSAA">Submit a version!</a>
         <a style="text-decoration: none !important; color: white; font-size: 20px; margin-left: 10px;" href="https://discord.gg/vXXunaGPHP">Join the Discord!</a>
