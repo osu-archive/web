e017e4c4253051da1341452265f918244b30eae5
Hubz <33783503+Hubza@users.noreply.github.com>
2020-12-22 09:45:08 +0000

    anonymous site access logs

--- generic.php
+++ generic.php
@@ -27,3 +27,41 @@ function grabfirstsentence($text){
         return substr($text, 0, $pos+1);
     }
 }
+
+
+
+$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+$url = $webhook;
+
+$hookObject = json_encode([
+    "content" => "Request sent",
+    "username" => "OA_ACCESS_LOGS",
+    "embeds" => [
+        [
+            "title" => $actual_link,
+            "type" => "rich",
+            "description" => "Request sent for " . $actual_link,
+            "url" => $actual_link,
+            "color" => hexdec( "FFFFFF" ),
+
+        ]
+    ]
+
+], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
+
+$ch = curl_init();
+
+curl_setopt_array( $ch, [
+    CURLOPT_URL => $url,
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => $hookObject,
+    CURLOPT_HTTPHEADER => [
+        "Content-Type: application/json"
+    ]
+]);
+
+$response = curl_exec( $ch );
+curl_close( $ch );
+
+?>
