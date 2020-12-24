43ddfb77a83effa3d710c5135db64a7f9a6c0139
Hubz <33783503+Hubza@users.noreply.github.com>
2020-12-24 17:21:19 +0000

    add dev warning

--- footer.php
+++ footer.php
@@ -1,9 +1,19 @@
 <div class="footer">
     <div class="footer-content">
         © UNTONE 2020
+        <?php
+        if($dev == 1){
+            ?>
+        <br>
+
+        This site is purely for development. Things may break.
+        <?php
+        }
+        ?>
         <div class="fc-left">
             <a class="ft-link" href="https://github.com/osu-archive/web/issues/new">Report a bug</a>
 
         </div>
     </div>
+   
 </div>
