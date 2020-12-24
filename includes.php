2855f30ef2541d57d5226a57d077dc94d5b2a5c5
Hubz <33783503+Hubza@users.noreply.github.com>
2020-12-24 14:29:17 +0000

    fixed something

--- includes.php
+++ includes.php
@@ -1,3 +1,4 @@
 <link rel="canonical" href="https://archive.osu.hubza.co.uk" />
-<link rel="stylesheet" href="main_new.css">
+
+<link rel="stylesheet" href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/main_new.css">
     <script src="https://kit.fontawesome.com/91ad005f46.js" crossorigin="anonymous"></script>
