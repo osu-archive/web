21f822502a8e9679f789e3ee669ef609c3833d60
Hubz <33783503+Hubza@users.noreply.github.com>
2020-12-26 00:44:00 +0000

    update development footer warning design

--- footer.php
+++ footer.php
@@ -1,19 +1,23 @@
 <div class="footer">
     <div class="footer-content">
         © UNTONE 2020
-        <?php
-        if($dev == 1){
-            ?>
-        <br>
-
-        This site is purely for development. Things may break.
-        <?php
-        }
-        ?>
         <div class="fc-left">
             <a class="ft-link" href="https://github.com/osu-archive/web/issues/new">Report a bug</a>
 
         </div>
     </div>
-   
+    <?php
+        if($dev == 1){
+            ?>
+    <div class="wip-warning">
+        <p class="wip-text">This site is purely for development. Things may break.</p>
+        <div class="fc-left">
+            <a class="ft-link" href="https://archive.osu.hubza.co.uk">Go to stable site</a>
+
+        </div>
+    </div>
+    <?php
+        }
+        ?>
+
 </div>
