b360fc9910d0fd9bb8e9b7c6491f516ea0553cd2
Hubz <33783503+Hubza@users.noreply.github.com>
2020-12-28 17:55:06 +0000

    clean up

--- admin/api_edit_version.php
+++ admin/api_edit_version.php
@@ -5,7 +5,6 @@ include("admin_generic.php");
 include("../config.php");
 include("../generic.php");
 
-
 session_start();
 
 if(!isset($_SESSION['role'])){
@@ -14,7 +13,7 @@ if(!isset($_SESSION['role'])){
     exit;
 }
 
-
+$good = false;
 
 if(isset($_FILES['fileToUpload'])){
     $path = $_FILES['fileToUpload']['name'];
@@ -22,7 +21,12 @@ if(isset($_FILES['fileToUpload'])){
     echo $_FILES['fileToUpload']['error'];
 
     $ext = pathinfo($path, PATHINFO_EXTENSION);
+
+    $ext = substr($path, strrpos($path, '.') + 1);
+
     $good;
+    echo "EXT:" . $ext;
+
     if($ext == "7z"){
         $good = true;
     }
@@ -31,10 +35,8 @@ if(isset($_FILES['fileToUpload'])){
     }
    
     $version_temp = "https://archive.osu.hubza.co.uk/admin/temp/" . $_POST['version'] . "." . $ext;
-    echo $version_temp;
 
     $version_final = "https://upload.hubza.co.uk/osuarchive/user/" . $_POST['version'] . "." . $ext;
-    echo $version_final;
 
     if ($_FILES["fileToUpload"]["size"] > 100000000) {
         $good == false;
@@ -75,9 +77,6 @@ else{
     $sql = "SELECT * FROM versions WHERE `ID` = '" . $id . "'";
 }
 
-
-
-
 $sqlfinal = $db->query($sql);
 while($val = $sqlfinal->fetch_assoc()) {
     $version = $val;
@@ -98,7 +97,6 @@ $orig_archiver = $version['Archiver'];
 $orig_hidden = $version['hidden'];
 $orig_updates = $version['autoupdate'];
 $orig_supporter = $version['needssupporter'];
-
 
 if(isset($_POST['version'])){  
     $version = $_POST['version'];
@@ -228,7 +226,6 @@ if(!isset($desc)){
         $desc = $orig_desc;
     }
 }
-
 
 if(!isset($descshort)){
     if(isset($orig_descshort)){
