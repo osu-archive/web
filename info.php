1fa360ac3a5c3b46334edd81f070337ea54769f1
Hubz <33783503+Hubza@users.noreply.github.com>
2021-01-03 01:12:27 +0000

    add more clean download error

--- info.php
+++ info.php
@@ -21,6 +21,15 @@ while($val = $sqlfinal->fetch_assoc()) {
     $en = $val;
     $viewCount = $val['Views'];
     $desc = grabfirstsentence($val['VersionInfo']);
+
+    
+    $file = $val['OADL-URL'];
+    
+    if($file == ""){
+    
+        $file = $val['GDDL-URL'];
+    
+    }
 }
 
 
@@ -47,34 +56,35 @@ include("navbar.php");
     <!-- Primary Meta Tags -->
     <title>osu!archive • version - <?php echo $en['Name']; ?> (<?php echo $en['Version']; ?>)</title>
     <meta name="title" content="osu!archive • version - <?php echo $en['Name']; ?> (<?php echo $en['Version']; ?>)">
-    <meta name="description"
-        content="<?php echo $desc; ?>">
+    <meta name="description" content="<?php echo $desc; ?>">
 
     <!-- Open Graph / Facebook -->
     <meta property="og:type" content="website">
     <meta property="og:url" content="https://archive.osu.hubza.co.uk/">
-    <meta property="og:title" content="osu!archive • version - <?php echo $en['Name']; ?> (<?php echo $en['Version']; ?>)">
-    <meta property="og:description"
-        content="<?php echo $desc; ?>">
+    <meta property="og:title"
+        content="osu!archive • version - <?php echo $en['Name']; ?> (<?php echo $en['Version']; ?>)">
+    <meta property="og:description" content="<?php echo $desc; ?>">
     <meta property="og:image" content="<?php echo $screenshot; ?>">
 
     <!-- Twitter -->
     <meta property="twitter:card" content="summary_large_image">
     <meta property="twitter:url" content="https://archive.osu.hubza.co.uk/">
-    <meta property="twitter:title" content="osu!archive • version - <?php echo $en['Name']; ?> (<?php echo $en['Version']; ?>)">
-    <meta property="twitter:description"
-        content="<?php echo $desc; ?>">
+    <meta property="twitter:title"
+        content="osu!archive • version - <?php echo $en['Name']; ?> (<?php echo $en['Version']; ?>)">
+    <meta property="twitter:description" content="<?php echo $desc; ?>">
     <meta property="twitter:image" content="<?php echo $screenshot; ?>">
 
     <meta name="viewport" content="width=device-width, initial-scale=0.6 ">
-    <meta name="keywords" content="osu, old, version, osugame, archive, archival, hubz, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020">
+    <meta name="keywords"
+        content="osu, old, version, osugame, archive, archival, hubz, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020">
 </head>
 
 <div class="page panel">
     <div class="ver-cont">
         <div class="v-header">
             <div class="vh-top">
-                <p class="vh-text"><span style="font-weight: 200; ">Versions / </span> <?php echo htmlspecialchars(addslashes($_GET['v'])); ?></p>
+                <p class="vh-text"><span style="font-weight: 200; ">Versions / </span>
+                    <?php echo htmlspecialchars(addslashes($_GET['v'])); ?></p>
             </div>
         </div>
         <?php
@@ -87,7 +97,8 @@ while($val = $sqlfinal->fetch_assoc()) {
             <div class="ver-panel-header" style="background-image: url(<?php echo $screenshot; ?>)">
                 <div class="blur-cont">
                     <div class="bc-left">
-                        <p class="versionname"><?php echo $val['Name']; ?> <span class="vn-thin"><?php echo $val['Version']; ?></span></p>
+                        <p class="versionname"><?php echo $val['Name']; ?> <span
+                                class="vn-thin"><?php echo $val['Version']; ?></span></p>
                         <p class="bc-date"><?php echo date("F jS, Y", strtotime($val['ReleaseDate'])); ?></p>
                         <p class="bc-archiver">archived by <a class="bca-name"><?php echo $val['Archiver']; ?></a></p>
                     </div>
@@ -112,7 +123,8 @@ $usewarning = false;
                     ?>
                 <div class="warning">
                     <i class="fas fa-exclamation-circle"></i>
-                    <p class="warning-text">This version automatically updates when starting. Learn how to disable auto-update</p>
+                    <p class="warning-text">This version automatically updates when starting. Learn how to disable
+                        auto-update</p>
                 </div>
                 <div class="warning">
                     <i class="fas fa-exclamation-circle"></i>
@@ -120,13 +132,27 @@ $usewarning = false;
                 </div>
                 <?php
                 }
-                ?>  
+                ?>
                 <?php if($val['VersionInfo'] != ""){ ?>
-                <p class="vpc-description <?php if($usewarning == true) { echo "vpcd-paddingtop"; } ?>"><?php echo $val['VersionInfo']; ?></p>
+                <p class="vpc-description <?php if($usewarning == true) { echo "vpcd-paddingtop"; } ?>">
+                    <?php echo $val['VersionInfo']; ?></p>
                 <?php
                 }
                 ?>
-                <a class="vpc-download-button" href="https://archive.osu.hubza.co.uk/download?v=<?php echo $val["Version"]; ?>"><i class="fas fa-download"></i><p class="db">Download</p></a>
+                <?php 
+                if(checkOnline($file) == true){ ?>
+                <a class="vpc-download-button"
+                    href="https://archive.osu.hubza.co.uk/download?v=<?php echo $val["Version"]; ?>"><i
+                        class="fas fa-download"></i>
+                    <p class="db">Download</p>
+                </a>
+                <?php } else { ?>
+                <div class="warning" style="filter: hue-rotate(-46deg) saturate(2);">
+                    <i class="fas fa-exclamation-circle"></i>
+                    <p class="warning-text">The download for this version is currently unavailable, please check back
+                        later.</p>
+                </div>
+                <?php } ?>
             </div>
         </div>
         <div class="screenshots">
@@ -134,12 +160,13 @@ $usewarning = false;
             $screenshots = grabshots($val);
                 foreach ($screenshots as $value) {
                     ?>
-                    <img src="
+            <img src="
                     <?php
                     echo $value;
                     ?>
-                    " class="ss-screenshot" onerror="this.src='https://archive.osu.hubza.co.uk/img/screenshot_error.png'">
-                    <?php
+                    " class="ss-screenshot"
+                onerror="this.src='https://archive.osu.hubza.co.uk/img/screenshot_error.png'">
+            <?php
                 }
             ?>
         </div>
@@ -148,4 +175,3 @@ $usewarning = false;
 ?>
     </div>
 </div>
-
