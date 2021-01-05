17008e8dc27f771770a02a33f3e46113144822e1
Hubz <33783503+Hubza@users.noreply.github.com>
2021-01-05 22:03:24 +0000

    move favicons to /favi

--- navbar.php
+++ navbar.php
@@ -1,11 +1,11 @@
-<link id="favi" rel="icon" href="/favicon.svg">
-<link rel="apple-touch-icon" sizes="180x180" href="/favicon-touch.svg">
-<link rel="apple-touch-icon" sizes="256x256" href="/favicon-touch.svg">
-<link rel="icon" type='image/png' sizes='256x256' href="/favicon-touch.svg"/>
-<link rel="mask-icon" href="/favicon.svg" color="#0090DE">
+<link id="favi" rel="icon" href="favi/favicon.svg">
+<link rel="apple-touch-icon" sizes="180x180" href="favi/favicon-touch.svg">
+<link rel="apple-touch-icon" sizes="256x256" href="favi/favicon-touch.svg">
+<link rel="icon" type='image/png' sizes='256x256' href="favi/favicon-touch.svg"/>
+<link rel="mask-icon" href="favi/favicon.svg" color="#0090DE">
 <meta name="msapplication-TileColor" content="#0090DE">
 <meta name="theme-color" content="#0090DE"> 
-<link rel="shortcut icon" type="image/png" sizes="256x256" href="favicon-touch.png">
+<link rel="shortcut icon" type="image/png" sizes="256x256" href="favi/favicon-touch.png">
 
 <?php
 $f_contents = file("splash.txt"); 
